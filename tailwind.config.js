18a44da9cf785bc2ec14d1c116a80d62f087dabc
Angeline Marcelle Lukito <87764018+angelinemarcelle@users.noreply.github.com>
2024-05-12 22:44:39 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+      backgroundColor :{
+        'dark-blue' : '#000957',
+        'dark-blue-hover' : '#03139e',
+        'light-blue' : '#1D77FF',
+        'light-grey' : '#FFFFFF',
+      },
+      backgroundOpacity: {
+        '75' : '0.75',
+        '40' : '0.4',
+      }
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem'
+    },
+  },
+  plugins: [],
+}
