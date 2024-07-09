e0845f79850796d4fc8c22fca96df617678f7f95
Angeline Marcelle Lukito <87764018+angelinemarcelle@users.noreply.github.com>
2024-07-10 01:52:15 +0800

    deleted unnecessary imgs and edited dates

--- frontend/src/pages/events_page.js
+++ frontend/src/pages/events_page.js
@@ -8,28 +8,28 @@ function EventPage() {
   const upcomingEvents = [
     {
       title: "Marketing Strategy Seminar",
-      date: "Sep 15",
+      date: "Jul 11",
       location: "Marketing Department",
       description: "Seminar on the latest marketing strategies and trends.",
       image: "https://d19d5sz0wkl0lu.cloudfront.net/dims4/default/59e5ecf/2147483647/resize/800x%3E/quality/90/?url=https%3A%2F%2Fatd-brightspot.s3.amazonaws.com%2F77%2Fd1%2F62bd27e34677a8d4ee0dad57990e%2Fadobestock-209215591.jpeg",
     },
     {
       title: "Employee Wellness Workshop",
-      date: "Sep 18",
+      date: "Jul 28",
       location: "Health and Wellness Center",
       description: "Workshop focused on employee health and wellness.",
       image: "https://www.samyama.org/wp-content/uploads/2023/01/Corporate-Wellness-SAMYAMA.jpg",
     },
     {
       title: "Data Analysis Training",
-      date: "Sep 21",
+      date: "Aug 2",
       location: "Data Science Lab",
       description: "Training session on data analysis techniques and tools.",
       image: "https://microsoft-office-courses.co.uk/wp-content/uploads/2019/10/shutterstock_701604637-min.jpg",
     },
     {
       title: "Annual Company Outing",
-      date: "Sep 25",
+      date: "Aug 17",
       location: "Resort",
       description: "An off-site retreat for employees to relax and bond.",
       image: "https://cdn.prod.website-files.com/61eb3f79cfe8dd4bf6350818/63ff7b38bcf6a055318acdfd_PACE-EmmaCrystalWorkshop-2-scaled.webp",
@@ -39,42 +39,42 @@ function EventPage() {
   const pastEvents = [
     {
       title: "Office BBQ Party",
-      date: "Aug 25",
+      date: "May 25",
       location: "Office Terrace",
       description: "An office gathering with BBQ and fun activities.",
       image: "https://www.postable.com/blog/wp-content/uploads/2016/06/dinner-party-barbecue-and-roast-pork-at-night-picture-id931644570.jpg",
     },
     {
       title: "Sales Training",
-      date: "Aug 28",
+      date: "May 28",
       location: "Sales Department",
       description: "Training session for the sales team to improve their techniques.",
       image: "https://elearningindustry.com/wp-content/uploads/2021/08/LMS-For-Corporate-Training-Find-Your-Perfect-Fit.png",
     },
     {
       title: "IT Security Awareness Workshop",
-      date: "Sep 2",
+      date: "Jun 2",
       location: "IT Department",
       description: "Workshop to educate employees about IT security practices.",
       image: "https://www.secion.de/assets/images/9/secion-cyber-security-workshop-01-816bbe24.jpg",
     },
     {
       title: "Customer Service Excellence Training",
-      date: "Sep 5",
+      date: "Jun 5",
       location: "Customer Service Department",
       description: "Training session for customer service representatives.",
       image: "https://cdn.prod.website-files.com/5f55ff47b6d23a11cb496a69/63f8e9acf899f7545da43421_12%20benefits%20of%20employee%20training%20%26%20development.jpg",
     },
     {
       title: "Office Happy Hour",
-      date: "Sep 8",
+      date: "Jun 28",
       location: "Office Lounge",
       description: "A casual gathering for employees to unwind after work.",
       image: "https://blog.thriver.com/wp-content/uploads/2022/01/blog_office-happy-hour-ideas-scaled.jpg",
     },
     {
       title: "Project Management Workshop",
-      date: "Sep 11",
+      date: "Jul 8",
       location: "Project Management Office",
       description: "Workshop on effective project management techniques.",
       image: "https://mytechdecisions.com/wp-content/uploads/2018/09/Conference_Collaboration_Meeting.jpg",
@@ -99,6 +99,10 @@ function EventPage() {
           ))}
         </div>
 
+        <h2 className="text-3xl font-light mt-16 mb-4">Register Here</h2>
+        <hr className="mb-6"/>
+        <RSVPForm upcomingEvents={upcomingEvents} />
+
         <h2 className="text-3xl font-light mt-16 mb-4">Past Events</h2>
         <hr className="mb-6"/>
         <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4 mb-16">
@@ -106,10 +110,6 @@ function EventPage() {
             <EventCard key={index} event={event} />
           ))}
         </div>
-
-        <h2 className="text-3xl font-light mt-16 mb-4">Register Here</h2>
-        <hr className="mb-6"/>
-        <RSVPForm upcomingEvents={upcomingEvents} />
       </div>
       
     </div>
